6715a1d53ac3c657e90ab5dd58a1dd1fb3eea4c9
Manasa <manasabellam25@gmail.com>
2025-05-22 11:20:29 +0530

    checking if jenkins got triggered when pushed chnages into the github(is wording correct, i dont know)

--- src/test/java/testcases/ColorsPageTest.java
+++ src/test/java/testcases/ColorsPageTest.java
@@ -20,7 +20,7 @@ public class ColorsPageTest extends BaseTest {
 
         // Verify selected options
         List<WebElement> selectedOptions = colorsPage.getSelectedColors();
-        Assert.assertEquals(selectedOptions.size(), 3, "Should select 3 colors");
+        Assert.assertEquals(selectedOptions.size(), 3, "Should  select 3 colors");
 
         // Assert that each selected color is indeed selected
         Assert.assertTrue(selectedOptions.stream().anyMatch(e -> e.getText().equals("Red")), "color red ,Red should be selected");
